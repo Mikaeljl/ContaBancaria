35e8205bca8e14e736f9e32010699ed8f0adceb9
Mikael de Jesus <mikaeljl824@gmail.com>
2024-09-12 19:50:06 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class ContaBancaria
+    {
+        ContaBancaria contaBancaria = new ContaBancaria();
+
+        ContaBancaria.DefinirLimiteSaque(1000):
+        ContaBancaria.Depositar(1000);
+        ContaBancaria.Sacar(200)/;
+        Console.WriteLine("o saldo total é:" + saldo);
+            //["deposito de mil reais realizado]",saque de 200 reais realizado"]
+            Console.Readline();
+            
+
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+public class ContaBancaria
+{
+    private decimal saldo;
+
+    public decimal Saldo { get { return saldo; }
+
+        
+    }
+
+    private List<string> historicotransacoes = new List<string>();
+
+    private decimal limitesaquediario;
+
+   
+    public void Depositar(decimal valor)
+    {
+        saldo += valor;
+        historicotransacoes.Add("Deposito de {valor} realizado");
+    }
+
+    public void sacar(decimal valor)
+    {
+        if (valor < limitesaquediario)
+        {
+            saldo -= valor;
+            Console.WriteLine("Saque realizado com sucesso!" + valor);
+
+        }
+
+        else
+        {
+            Console.WriteLine("limite de saques diarios alcançado!");
+        }
+
+    }
+
+    public void DefinirSaqueDiario(decimal limite)
+    {
+        limitesaquediario = limite;
+    }
+
+    public void exibirhistorico()
+    {
+        foreach (string h in historicotransacoes)
+        {
+            Console.WriteLine(h);
+        }
+    }
+}
